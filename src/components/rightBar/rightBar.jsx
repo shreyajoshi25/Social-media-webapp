49aa70729b687f001eb03e662abfeb9989ecb876
Shreya Joshi <joshishreya2502@gmail.com>
2024-01-26 21:48:48 +0530

    changes

--- src/components/rightBar/rightBar.jsx
+++ src/components/rightBar/rightBar.jsx
@@ -6,7 +6,7 @@ const RightBar = () => {
     <div className="rightBar">
       <div className="container">
         <div className="item">
-          <span>Suggestions for You</span>
+          <span className='bold'>Suggestions for You</span>
           <div className="user">
             <div className="userInfo">
               <img src="https://media.licdn.com/dms/image/D4E03AQEtpEmW1OWfng/profile-displayphoto-shrink_800_800/0/1667319071310?e=2147483647&v=beta&t=k8SL_mApYLwX13cKc7OsHTeUlXyqzibx5PGwyvxeGHs" alt="" />
@@ -30,7 +30,7 @@ const RightBar = () => {
         </div>
 
         <div className="item">
-          <span>Latest Activities</span>
+          <span className='bold'>Latest Activities</span>
 
           <div className="user">
             <div className="userInfo">
@@ -78,7 +78,7 @@ const RightBar = () => {
         </div>
 
         <div className="item">
-          <span>Online Friends</span>
+          <span className='bold'>Online Friends</span>
           <div className="user">
             <div className="userInfo">
               <img src="https://media.licdn.com/dms/image/D4E03AQEtpEmW1OWfng/profile-displayphoto-shrink_800_800/0/1667319071310?e=2147483647&v=beta&t=k8SL_mApYLwX13cKc7OsHTeUlXyqzibx5PGwyvxeGHs" alt="" />
